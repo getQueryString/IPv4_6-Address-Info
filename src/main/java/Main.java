7a92fde43b5debdebce8908c09568f2b766c6349
BuildTools <unconfigured@null.spigotmc.org>
2021-05-13 00:25:14 +0200

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,10 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.net.URL;
+import java.io.File;
+import java.io.IOException;
 
-public class Main extends JPanel {
+public class Main {
 
     public static JPanel loginpanel;
     public static JLabel userLabel;
@@ -32,22 +31,24 @@ public class Main extends JPanel {
         loginpanel.setLayout(null);
         JMenuBar bar = new JMenuBar();
         JFrame frame = new JFrame("IPv4/6-Address-Info");
-        frame.setSize(580, 270);
+        frame.setSize(540, 270);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(loginpanel);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
-        Image image = ImageIO.read(new File("IPInfo-BackgroundImage.jpg"));
-        JLabel img = new JLabel();
-        img.setIcon(new ImageIcon(image));
-        frame.add(img);
+        frame.getRootPane().setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.LIGHT_GRAY));
+        //frame.setContentPane(new BackGroundPane("src/main/resources/IPInfo-BackgroundImage.jpg"));
 
         // JMenu
         JMenu menu = new JMenu("Info");
+        menu.setFont(new Font("Courier New", Font.BOLD, 12));
         bar.add(menu);
 
         JMenuItem menuItem = new JMenuItem("Source code");
         menuItem.addActionListener(new Menu());
+        menuItem.setFont(new Font("Courier New", Font.BOLD, 12));
+        menuItem.setBackground(Color.YELLOW);
+        menuItem.setForeground(Color.BLUE);
         menu.add(menuItem);
 
         frame.setJMenuBar(bar);
@@ -55,43 +56,54 @@ public class Main extends JPanel {
         // JPanel_Login
         userLabel = new JLabel("Username:");
         userLabel.setBounds(10, 20, 80, 25);
+        userLabel.setFont(new Font("Courier New", Font.BOLD, 14));
+        userLabel.setForeground(Color.RED);
         loginpanel.add(userLabel);
 
         userText = new JTextField(20);
         userText.setBounds(100, 20, 165, 25);
+        userText.setBackground(Color.BLACK);
+        userText.setForeground(Color.YELLOW);
         loginpanel.add(userText);
 
         pwLabel = new JLabel("Password:");
         pwLabel.setBounds(10, 50, 80, 25);
+        pwLabel.setForeground(Color.RED);
         loginpanel.add(pwLabel);
 
         pwText = new JPasswordField();
         pwText.setBounds(100, 50, 165, 25);
+        pwText.setBackground(Color.BLACK);
+        pwText.setForeground(Color.YELLOW);
         loginpanel.add(pwText);
 
-        button = new JButton("Login");
-        button.setBounds(10, 80, 80, 25);
+        button = new JButton("Login", new ImageIcon("src/main/resources/LoginIcon.png"));
+        button.setBounds(10, 80, 90, 25);
         button.addActionListener(new Login());
         loginpanel.add(button);
 
         denied = new JLabel("Access denied!");
         denied.setBounds(10, 110, 100, 25);
+        denied.setForeground(Color.RED);
         denied.setVisible(false);
         loginpanel.add(denied);
 
         // JPanel_Track
         ipaddrlabel = new JLabel("IPv4/6-Address:");
         ipaddrlabel.setBounds(10, 20, 100, 25);
+        ipaddrlabel.setForeground(Color.RED);
         ipaddrlabel.setVisible(false);
         loginpanel.add(ipaddrlabel);
 
         ipaddrText = new JTextField(64);
         ipaddrText.setBounds(120, 20, 165, 25);
+        ipaddrText.setBackground(Color.BLACK);
+        ipaddrText.setForeground(Color.YELLOW);
         ipaddrText.setVisible(false);
         loginpanel.add(ipaddrText);
 
-        ipaddrbutton = new JButton("Track");
-        ipaddrbutton.setBounds(10, 50, 80, 25);
+        ipaddrbutton = new JButton("Track", new ImageIcon("src/main/resources/StartIcon.png"));
+        ipaddrbutton.setBounds(10, 50, 90, 25);
         ipaddrbutton.setVisible(false);
         ipaddrbutton.addActionListener(new TrackIP());
         loginpanel.add(ipaddrbutton);
@@ -120,6 +132,12 @@ public class Main extends JPanel {
         loc.setBounds(10, 160, 400, 25);
         loc.setVisible(false);
         loginpanel.add(loc);
+
+        ip.setForeground(Color.BLUE);
+        hostname.setForeground(Color.ORANGE);
+        orga.setForeground(Color.GREEN);
+        location.setForeground(Color.RED);
+        loc.setForeground(Color.CYAN);
 
         frame.setVisible(true);
 
